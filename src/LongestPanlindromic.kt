7d29a035fdd725b00dbac26407c0b264e254f6ab
chisunjoung <csjoung@gmail.com>
2019-01-23 14:19:01 +0900

    LongestPanlindromic

---
+++ src/LongestPanlindromic.kt
@@ -0,0 +1,42 @@
+package prob5
+//Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+//
+//Example 1:
+//
+//Input: "babad"
+//Output: "bab"
+//Note: "aba" is also a valid answer.
+//Example 2:
+//
+//Input: "cbbd"
+//Output: "bb"
+
+
+class Solution {
+    fun longestPalindrome(s: String): String {
+        val l = s.length
+        var longPalindrome = ""
+        for (i in 0..(l-1)){
+            if( (l-i) <=longPalindrome.length ) break
+            for (j  in l downTo i){
+
+                if ((j-i) <= longPalindrome.length ) break
+                var forward_s = s.substring(i,j)
+                var backward_s = forward_s.reversed()
+                if (backward_s == forward_s && longPalindrome.length < forward_s.length)
+                    longPalindrome = forward_s
+            }
+
+        }
+
+        return longPalindrome
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val s = "cbbd"
+
+
+    println("ans " + Solution().longestPalindrome(s))
+}
