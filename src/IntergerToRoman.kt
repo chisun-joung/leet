99d643f8471e52379acc48bc12b85b2fae135237
chisunjoung <csjoung@gmail.com>
2019-02-14 10:38:09 +0900

    IntergerToRoman

--- src/IntergerToRoman.kt
+++ src/IntergerToRoman.kt
@@ -1,6 +1,8 @@
 package prob12
 
+import org.junit.Test
 import java.lang.StringBuilder
+import kotlin.test.assertEquals
 
 //Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 //
@@ -49,14 +51,80 @@ class Solution {
         if (num < 1 || num > 3999) return ""
         var temp = num
         var result = StringBuilder()
+        var roman = temp / 1000
+        temp %= 1000
 
-        when(temp%10){
+        when(roman){
+            1 -> result.append("M")
+            2 -> result.append("MM")
+            3 -> result.append("MMM")
+            else -> ""
+        }
+
+        roman = temp / 100
+        temp = temp % 100
+
+        when(roman){
+            1 -> result.append("C")
+            2 -> result.append("CC")
+            3 -> result.append("CCC")
+            4 -> result.append("CD")
+            5 -> result.append("D")
+            6 -> result.append("DC")
+            7 -> result.append("DCC")
+            8 -> result.append("DCCC")
+            9 -> result.append("CM")
+            else -> ""
+        }
+
+        roman = temp / 10
+        temp = temp % 10
+
+        when(roman){
+            1 -> result.append("X")
+            2 -> result.append("XX")
+            3 -> result.append("XXX")
+            4 -> result.append("XL")
+            5 -> result.append("L")
+            6 -> result.append("LX")
+            7 -> result.append("LXX")
+            8 -> result.append("LXXX")
+            9 -> result.append("XC")
+            else -> ""
+        }
+
+
+
+
+        when(temp){
             1 -> result.append("I")
             2 -> result.append("II")
             3 -> result.append("III")
-            4 -> result.
+            4 -> result.append("IV")
+            5 -> result.append("V")
+            6 -> result.append("VI")
+            7 -> result.append("VII")
+            8 -> result.append("VIII")
+            9 -> result.append("IX")
+            else -> ""
         }
 
-        return ""
+
+        return result.toString()
+    }
+}
+
+
+class Tests {
+    @Test fun testroman1(){
+        assertEquals("III", Solution().intToRoman(3))
+    }
+
+    @Test fun testroman2(){
+        assertEquals("LVIII", Solution().intToRoman(58))
+    }
+
+    @Test fun testroman3(){
+        assertEquals("MCMXCIV", Solution().intToRoman(1994))
     }
 }
